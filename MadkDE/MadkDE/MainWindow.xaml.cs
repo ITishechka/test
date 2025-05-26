6a81ec3de51e26ba8e3b5ab0021655f8a180fe27
ITishechka <141069891+ITishechka@users.noreply.github.com>
2025-05-26 23:56:19 +0300

    Add files via upload

---
+++ MadkDE/MadkDE/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Media.Imaging;
+using MadkDE.Models;
+
+namespace MadkDE.Views
+{
+    public partial class MainWindow : Window
+    {
+        private readonly User _user;
+
+        public MainWindow(User user)
+        {
+            InitializeComponent();
+            _user = user;
+            this.Title = "Управление Материалами";
+            this.Icon = new BitmapImage(new Uri("pack://application:,,,/Resources/AppIcon.ico"));
+            LoadMaterials();
+        }
+
+        private void LoadMaterials()
+        {
+            using var db = new MadkContext();
+            var materials = db.Materials.Include(m => m.MaterialType).ToList();
+            MaterialsGrid.ItemsSource = materials;
+        }
+
+        private void OpenSuppliers_Click(object sender, RoutedEventArgs e)
+        {
+            var win = new SuppliersWindow();
+            win.ShowDialog();
+        }
+
+        private void EditMaterial_Click(object sender, RoutedEventArgs e)
+        {
+            if (_user.Role != "Администратор")
+            {
+                MessageBox.Show("Доступ запрещён: только для администратора.");
+                return;
+            }
+
+            var selectedMaterial = MaterialsGrid.SelectedItem as Material;
+            if (selectedMaterial != null)
+            {
+                var editor = new EditMaterialWindow(selectedMaterial);
+                editor.ShowDialog();
+                LoadMaterials();
+            }
+            else
+            {
+                MessageBox.Show("Выберите материал для редактирования.");
+            }
+        }
+    }
+}
